40a968f58cfa8ea3b045b2d7ab12c4c371a2d283
Timo <dte0011@edu.curio.nl>
2025-03-12 10:07:33 +0100

    Alle page fixed
    Update

--- index.php
+++ index.php
@@ -1,15 +1,60 @@
+<?php require_once __DIR__.'/backend/config.php'; ?>
 <!doctype html>
 <html lang="nl">
 
 <head>
-    <title></title>
-    <?php require_once 'head.php'; ?>
+    <title>Placeholder</title>
 </head>
 
 <body>
-    
-    <div class="container">
 
+    <?php require_once __DIR__.'/resources/views/components/header.php'; ?>
+
+    <div class="container">
+        <h1>Placeholder</h1>
+        <a href="create.php">Placeholder &gt;</a>
+
+        <?php if(isset($_GET['msg'])) 
+        {
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+        } ?>
+
+        <?php
+            require_once '../backend/config/conn.php';
+
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <table>
+            <tr>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+                <th>Placeholder</th>
+            </tr>
+
+            <?php foreach($Placeholder as $Placeholder): ?>
+                <tr>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><?php echo $Placeholder['Placeholder']; ?></td>
+                    <td><a href="Placeholder.php?id=<?php echo $Placeholder['Placeholder']; ?>">Placeholder</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
 
     </div>
 
