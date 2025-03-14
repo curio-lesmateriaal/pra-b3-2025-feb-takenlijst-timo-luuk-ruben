cdfab50ebd6a733209c4a8274c5e7a757e0269e1
Luuk. <144131888+luuk1234567@users.noreply.github.com>
2025-03-14 11:28:10 +0100

    header
    ik heb de header aan gemaakt

---
+++ resources/views/components/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="header">
+        <a href="index.php">Index</a>
+        <a href="planbordController.php">Planbord</a>
+        <a href="Contact.php">Contact</a>
+    </div>
+</header>
