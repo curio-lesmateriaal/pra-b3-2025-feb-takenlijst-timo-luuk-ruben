829ae2947c3fc04a9cb35fcffc9a9dc475bfc931
Timo <dte0011@edu.curio.nl>
2025-03-17 12:24:46 +0100

    homepage remade + css
    heb de css gemaakt en main index/homepage reworked, tabel was niet nodig die staat al op de planbord home/index.

--- index.php
+++ index.php
@@ -3,71 +3,47 @@
 <html lang="nl">
 
 <head>
-    <title>Placeholder</title>
+    <title>Developerland - Takenlijst</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!--CSS-->
-    <link rel="stylesheet" href="../../public_html/css/main.css">
+    <!-- CSS -->
     <link rel="stylesheet" href="../../public_html/css/normalize.css">
+    <link rel="stylesheet" href="../../public_html/css/main.css">
 
-    <!--Fonts-->
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lavishly+Yours&family=Lexend:wght@100..900&family=Playwrite+IT+Moderna:wght@100..400&display=swap" rel="stylesheet">
-    <style>html {font-family: lato;}</style>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <!-- Header -->
+    <header>
+        <div class="header">
+            <a href="../../pra_b3/index.php">Home</a>
+            <a href="../../../pra_b3/pages/planbord/index.php">Planbord</a>
+            <a href="../../../pra_b3/pages/contact.php">Contact</a>
+        </div>
+    </header>
 
-    <?php require_once __DIR__.'/resources/views/components/header.php'; ?>
-
+    <!-- Main Content -->
     <div class="container">
-        <h1>Placeholder</h1>
-        <a href="create.php">Placeholder &gt;</a>
+        <h1>Welkom op Developerland Takenlijst</h1>
+        <p class="intro">Vind hier uw taken en blijf georganiseerd. Onze website helpt u om al uw taken efficiënt te beheren en bij te houden.</p>
+        <a href="create.php" class="button">Start met taken toevoegen &gt;</a>
 
-        <?php if(isset($_GET['msg'])) 
-        {
-            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
-        } ?>
-
-        <?php
-            require_once 'backend/conn.php';
-
-            $query = "SELECT * FROM taken";
-            $statement = $conn->prepare($query);
-            $statement->execute();
-            $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
-        ?>
-
-        <table>
-            <tr>
-                <th>Titel</th>
-                <th>Beschrijving</th>
-                <th>Afdeling</th>
-                <th>Status</th>
-                <th>Deadline</th>
-                <th>User</th>
-                <th>Created at</th>
-                <!--<th>Placeholder</th>-->
-                <!--<th>Placeholder</th>-->
-            </tr>
-
-            <?php foreach($taken as $taak): ?>
-                <tr>
-                    <td><?php echo $taak['titel']; ?></td>
-                    <td><?php echo $taak['beschrijving']; ?></td>
-                    <td><?php echo $taak['afdeling']; ?></td>
-                    <td><?php echo $taak['status']; ?></td>
-                    <td><?php echo $taak['deadline']; ?></td>
-                    <td><?php echo $taak['user']; ?></td>
-                    <td><?php echo $taak['created_at']; ?></td>
-                    <!--<td><?php echo $taak['Placeholder']; ?></td>-->
-                    <td><a href="Placeholder.php?id=<?php echo $Placeholder['Placeholder']; ?>">Placeholder</a></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-
+        <?php if(isset($_GET['msg'])): ?>
+            <div class="msg"><?php echo $_GET['msg']; ?></div>
+        <?php endif; ?>
     </div>
-    <?php require_once __DIR__.'/resources/views/components/footer.php'; ?>
+
+    <!-- Footer -->
+    <footer>
+        <p>&copy; 2025 Developerland</p>
+        <p>Neem contact op via <a href="mailto:info@developerland.nl">info@developerland.nl</a></p>
+        <p>Volg ons op <a href="#">Twitter</a> en <a href="#">LinkedIn</a></p>
+    </footer>
 </body>
 
 </html>
