1c89074214ccb342533fbaa5a96cd9fd5b5f94e6
Luuk. <144131888+luuk1234567@users.noreply.github.com>
2025-03-20 14:08:41 +0100

    merge conflict fix
    resolved merge conflict

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="header">
+        <nav>
+            <a href="../../index.php">Index</a>
+            <a href="../../../pra_b3/pages/planbord/planbord.php">Planbord</a>
+            <a href="../../../pra_b3/pages/contact.php">Contact</a>
+        </nav>
+    </div>
+</header>
