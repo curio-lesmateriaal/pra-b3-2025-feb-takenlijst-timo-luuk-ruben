c7c5c303199b43b619f744a4035fd45512118122
Timo <dte0011@edu.curio.nl>
2025-03-21 10:57:10 +0100

    Alle css resended met goede kleur
    juiste kleuren. Toegepast, blauw en groen

--- planbord.php
+++ planbord.php
@@ -5,11 +5,9 @@
 <head>
     <title>Planbord - Index</title>
 
-    <!--CSS-->
     <link rel="stylesheet" href="public_html/css/main.css">
     <link rel="stylesheet" href="public_html/css/normalize.css">
 
-    <!--Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lavishly+Yours&family=Lexend:wght@100..900&family=Playwrite+IT+Moderna:wght@100..400&display=swap" rel="stylesheet">
