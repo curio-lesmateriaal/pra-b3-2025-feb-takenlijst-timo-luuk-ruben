b269f41291f0c471d87c34bccc23252ca0149ad8
prodbymoonlight <D310325@edu.curio.nl>
2025-03-21 11:08:50 +0100

    Footer weer goed

--- resources/views/components/footer.php
+++ resources/views/components/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>placeholer</p>
-    <p>placeholder</p>
-    <p>placeholder</p>
+    <p>&copy; <?php echo date("Y"); ?> Developerland</p>
+    <p>Neem contact op via <a href="mailto:info@developerland.nl">info@developerland.nl</a></p>
+    <p>Volg ons op <a href="#">Twitter</a> en <a href="#">LinkedIn</a></p>
 </footer>
